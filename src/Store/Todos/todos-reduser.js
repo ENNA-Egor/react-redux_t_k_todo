992d8c3741caa2f41df8f03d9c059076a40894de
ENNA-Egor <e.yudakov@gmail.com>
2025-03-04 17:25:54 +0300

    Настроил createReducer на тудушки

--- src/Store/Todos/todos-reduser.js
+++ src/Store/Todos/todos-reduser.js
@@ -1,26 +1,44 @@
 // import {ADD_TODOS, TOGGLE_TODO, REMOVE_TODO} from './todos-const'
 import {addTodo, removeTodo, toggleTodo} from './todos-actions';
+import {createReducer} from '@reduxjs/toolkit';
 
-export const todosReducer = (state = [], action)=> {
-    switch (action.type){
-      case addTodo.toString():{
-        return [
-          ...state, 
-          {
-            ...action.payload
-          }
-        ];
-      }
-      case removeTodo.toString():{
-        return state.filter((todo)=> todo.id !== action.payload);
-      }
-      case  toggleTodo.toString():{
-        return state.map((todo)=>
-          todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo
-        );
-      }
-      default: {
-        return state;
-      }
-    }
-  }
+export const todosReducer = createReducer ([], (builder)=> {
+  builder
+    .addCase(addTodo, (state, action) => {
+      const newTodo = action.payload;
+      state.push(newTodo);
+    })
+    .addCase(removeTodo, (state, action) => {
+      const id = action.payload;
+      return state.filter((todo)=> todo.id !== id);
+    })
+    .addCase(toggleTodo, (state, action) => {
+      const id = action.payload;
+      const todo = state.find (todo => todo.id === id);
+      todo.completed = !todo.completed;
+    })
+})
+
+// export const todosReducer = (state = [], action)=> {
+//     switch (action.type){
+//       case addTodo.toString():{
+//         return [
+//           ...state, 
+//           {
+//             ...action.payload
+//           }
+//         ];
+//       }
+//       case removeTodo.toString():{
+//         return state.filter((todo)=> todo.id !== action.payload);
+//       }
+//       case  toggleTodo.toString():{
+//         return state.map((todo)=>
+//           todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo
+//         );
+//       }
+//       default: {
+//         return state;
+//       }
+//     }
+//   }
