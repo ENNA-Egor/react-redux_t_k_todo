2cfc065f0c050aef9c6a3b2336eaae6d7369e54f
ENNA-Egor <e.yudakov@gmail.com>
2025-03-05 08:29:22 +0300

    Настроил createReducer на counter

--- src/Store/Counter/counter-reduser.js
+++ src/Store/Counter/counter-reduser.js
@@ -1,18 +1,34 @@
 import {increment, decrement, reset} from './counter-actions';
+import {createReducer} from '@reduxjs/toolkit';
 
-export const counterReducer = (state = 0, action) => {
-    switch (action.type) {
-      case increment.toString(): {
-        return state + 1;
-      }
-      case decrement.toString(): {
-        return state - 1;
-      }
-      case reset.toString(): {
-        return 0;
-      }
-      default: {
-        return state;
-      }
-    }
-  };
+
+export const counterReducer = createReducer (0, (builder)=>{
+  builder
+      .addCase(increment, (state) =>{
+        // const count = action.payload;
+        return state  +1
+      })
+      .addCase(decrement, (state) =>{
+        // const count = action.payload;
+        return state  -1
+      })
+      .addCase(reset, (state) =>{
+        return state = 0
+      })
+})
+// export const counterReducer = (state = 0, action) => {
+//     switch (action.type) {
+//       case increment.toString(): {
+//         return state + 1;
+//       }
+//       case decrement.toString(): {
+//         return state - 1;
+//       }
+//       case reset.toString(): {
+//         return 0;
+//       }
+//       default: {
+//         return state;
+//       }
+//     }
+//   };
